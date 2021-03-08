6e358ba5fce65c70eaa44540efb994d9fd8fa21e
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-08 18:33:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,5 @@
 def game_hash 
-  = {
+  the_game_hash = {
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black","White"],
@@ -103,4 +103,4 @@ def game_hash
       ]
   }
 }
-game_hash 
+teh_game_hash 
